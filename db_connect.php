2ff408b8c19dd83f0c2702fbe193274563b88ad6
Vanessa Roycroft <vroyc1kf@mail.umw.edu>
2010-10-27 07:32:31 -0400

    added temporary home page

---
+++ db_connect.php
@@ -0,0 +1,6 @@
+<?php
+	$db = @mysqli_connect('localhost', 'restaurantuser', 'restaurants', 'restaurants');
+
+	$table = 'RestaurantInfo';
+	
+?>
---
+++ index.php
@@ -0,0 +1,20 @@
+<?php
+include "db_connect.php";
+?>
+
+<HTML>
+
+<style  TYPE="text/css">
+    BODY {
+	Background-color: #000000;
+}
+</style>
+
+<BODY>
+<CENTER>
+
+Welcome to the Restaurant Recommendation Website!
+
+</CENTER>
+</BODY>
+</HTML>
