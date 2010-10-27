e5dbbed1d1a6c975760a7040143b38e03b3a9830
Vanessa Roycroft <vroyc1kf@mail.umw.edu>
2010-10-27 09:31:44 -0400

    edited restaurantSearch

--- restaurantSearch.php
+++ restaurantSearch.php
@@ -1,9 +1,9 @@
 <?php 
 include "db_connect.php"; 
 
-	$band = mysqli_real_escape_string($db, trim($_POST['searchRestaurant'])); 
-	$query = "SELECT name FROM $table WHERE 
-		name LIKE '%$band%' ORDER BY name;";
+	$restaurant = mysqli_real_escape_string($db, trim($_POST['searchRestaurant'])); 
+	$query = "SELECT * FROM $table WHERE 
+		name LIKE '%$restaurant%' ORDER BY name;";
 
 	$result = mysqli_query($db, $query) 
 		or die("Error Querying Database"); 
@@ -12,29 +12,33 @@ $countrows = mysqli_num_rows($result);
 if ($countrows == 0) {
 	echo "<h1>No results matched your search!</h1>";
 } else {
-
-echo "<table boarder =1>";  
+ 
 
 	while($row = mysqli_fetch_array($result)) {
-		$id = $row['band_id'];
-		$name = $row['name']; 
-		$street_address = $row['street_address'];  
-		$city = $row['city']; 
+		$name = $row['name'];
+		$street_address = $row['street_address'];
+		$city = $row['city'];
 		$state = $row['state'];
-		$genre = $row['genre'];
-		$members = $row['members'];
+		$zip = $row['zip'];
+		$average_user_rating = $row['average_user_rating'];
+		$website = $row['website'];
+		$delivery = $row['delivery'];
 		
-echo "
-<b><h1><a href='index.php?page=bandpage.php&id=$id'>$name<h1></a></h1></b>
-<tr>
-<p><font size = '5'><b>Members: </b></font>$members </p>
-<p><font size = '5'><b>Location: </b></font>$street_address <br>$city, $state </p>
-<p><font size = '5'><b>Genres: </b></font>$genre </p>
-<br><br>
-</tr>\n"; 
+
+echo "<li>$name</li>";
+echo "<li>$street_address</li>";
+echo "<li>$city</li>";
+echo "<li>$state</li>";
+echo "<li>$zip</li>";
+echo "<li>$average_user_rating</li>";
+echo "<li>$website</li>";
+echo "<li>$delivery</li>";
+
 }
 }
-echo "</table>";
+
+
+
 
 mysqli_close($db); 
 ?>
