177abf9aa490dd1d1417413b415551936a4b73c8
unknown <Josh@.(none)>
2010-11-01 02:42:07 -0400

    Added error detection to db connect

--- db_connect.php
+++ db_connect.php
@@ -1,5 +1,11 @@
 <?php
-	$db = @mysqli_connect('localhost', 'user', 'Restaurants', 'Restaurants');
+	$db = mysqli_connect('localhost', 'user', 'Restaurants', 'Restaurants');
+
+	// Connection error detection
+	if (mysqli_connect_errno()) {
+		echo("Could not connect to database: ".mysqli_connect_error());
+		exit;
+	}
 
 	$table = 'RestaurantInfo';
 	
