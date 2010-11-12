65eb7c37e7a473ed512a82ef9d7c8425f4169c85
asams <asams@mail.umw.edu>
2010-11-11 17:25:53 -0800

    updated text

--- login.php
+++ login.php
@@ -11,12 +11,12 @@ if ($_GET['login'] == "yes") {
 ?>
 
 <center>
-<p><b>Login</b></p>
+<b><p><i><font size="5.5" face="Georgia" color="000066">L</font><font size="4.5" face="Georgia">ogin:</p></b></i></font>
 
 <form action="userProfile.php" method="post">
 <table border="0" cellpadding="3" cellspacing="0">
-<tr><td align="right"><b>Username: </b></td><td align="left"><input type="text" name="username" value=""></td</tr>
-<tr><td align="right"><b>Password: </b></td><td align="left"><input type="password" name="password" value=""></td</tr>
+<tr><td align="right"><b><i><font size="4.5" face="Georgia" color="CC6600">U</font><font size="3.5" face="Georgia">sername:</font></b></i></td><td align="left"><input type="text" name="username" value=""></td</tr>
+<tr><td align="right"><b><i><font size="4.5" face="Georgia" color="CC6600">P</font><font size="3.5" face="Georgia">assword:</font></b></i></td><td align="left"><input type="password" name="password" value=""></td</tr>
 <tr><td>&nbsp;</td><td align="center"><input type="submit" value="Login"></td></tr>
 </table>
 </form>
