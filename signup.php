b58780eba2331718255964a074cb67a9fd847123
asams <asams@mail.umw.edu>
2010-11-29 18:08:32 -0800

    gets all of the radio button values & inserts into databases

--- signup.php
+++ signup.php
@@ -10,11 +10,34 @@ if ($_GET['signup'] == "yes") {
 	$password = mysqli_real_escape_string($db, trim($_POST['password']));
 
 	$price = $_POST[group1];
+	$delivery = $_POST[group2];
+ 	$takeout = $_POST[group3];
+	$groups = $_POST[group4];
+ 	$reservations = $_POST[group5];
+	$outsideSeating = $_POST[group6];
+	$bar = $_POST[group7];
+	$kids = $_POST[group8];
+ 	$speed = $_POST[group9];
+	$steakhouse = $_POST[group10];
+	$american = $_POST[group11];
+	$middleEastern = $_POST[group12];
+	$asian = $_POST[group13];
+	$italian = $_POST[group14];
+ 	$chinese = $_POST[group15];
+	$japanese = $_POST[group16];
+	$indian = $_POST[group17];
+	$french = $_POST[group18];
+	$greek = $_POST[group19];
+	$mexican = $_POST[group20];
+ 	$vegetarian = $_POST[group21];
+	$seafood = $_POST[group22];
+
+
 
 	$errormess = "";
 
 	// Make Sure All Fields Are Entered
-	if ($first_name == "" || $last_name == "" || $username == "" || $password == "") {
+	if ($first_name == "" || $last_name == "" || $username == "" || $password == "" || $price == "" || $delivery == "" || $takeout == "" || $groups == "" || $reservations == "" || $outsideSeating == "" || $bar == "" || $kids == "" || $speed == "" || $steakhouse == "" || $american == "" || $middleEastern == "" || $asian == "" || $italian == "" || $chinese == "" || $japanese == "" || $indian == "" || $french == "" || $greek == "" || $mexican == "" || $vegetarian == "" || $seafood == "") {
 		$errormess = "Your must fill in all the fields";
 	}
 
@@ -27,7 +50,7 @@ if ($_GET['signup'] == "yes") {
 
 
 	if ($errormess == "") {
-		@mysqli_query($db, "Insert into `Users` (first_name, last_name, username, password, price) values ('$first_name', '$last_name', '$username', SHA('$password'), $price)");
+		@mysqli_query($db, "Insert into `Users` (first_name, last_name, username, password, price, delivery, takeout, accommodate_groups, reservations, outside_seating, bar, kids, fast_food, steakhouse_influence, american_influence, middle_eastern_influence, asian_influence, italian_influence, chinese_influence, japanese_influence, indian_influence, french_influence, greek_influence, mexican_influence, vegetarian_influence, seafood_influence) values ('$first_name', '$last_name', '$username', SHA('$password'), $price, $delivery, $takeout, $groups, $reservations, $outsideSeating, $bar, $kids, $speed, $steakhouse, $american, $middleEastern, $asian, $italian, $chinese, $japanese, $indian, $french, $greek, $mexican, $vegetarian, $seafood)");
 		echo("Account created successfully. <p><a href='login.php'>Login</a> to get started!</p>");
 		include("footer.php");
 		exit;
@@ -65,131 +88,131 @@ if ($_GET['signup'] == "yes") {
 
 
 <tr><td align="left">Do you prefer a restaurant that offers delivery?</td></tr>
-<tr><td align="left"><input type="radio" name="group2" value="yesDelivery"> Yes </td></tr>
-<tr><td align="left"><input type="radio" name="group2" value="noDelivery"> No </td></tr>
+<tr><td align="left"><input type="radio" name="group2" value="1"> Yes </td></tr>
+<tr><td align="left"><input type="radio" name="group2" value="0"> No </td></tr>
 
 <tr><td align="left">Do you prefer a restaurant that offers takeout?</td></tr>
-<tr><td align="left"><input type="radio" name="group3" value="yesTakeout"> Yes </td></tr>
-<tr><td align="left"><input type="radio" name="group3" value="noTakeout"> No </td></tr>
+<tr><td align="left"><input type="radio" name="group3" value="1"> Yes </td></tr>
+<tr><td align="left"><input type="radio" name="group3" value="0"> No </td></tr>
 
 <tr><td align="left">Do you prefer a restaurant that can accommodate groups?</td></tr>
-<tr><td align="left"><input type="radio" name="group4" value="yesGroups"> Yes </td></tr>
-<tr><td align="left"><input type="radio" name="group4" value="noGroup"> No </td></tr>
+<tr><td align="left"><input type="radio" name="group4" value="1"> Yes </td></tr>
+<tr><td align="left"><input type="radio" name="group4" value="0"> No </td></tr>
 
 <tr><td align="left">Do you prefer a restaurant that accepts reservations?</td></tr>
-<tr><td align="left"><input type="radio" name="group5" value="yesReservations"> Yes </td></tr>
-<tr><td align="left"><input type="radio" name="group5" value="noReservations"> No </td></tr>
+<tr><td align="left"><input type="radio" name="group5" value="1"> Yes </td></tr>
+<tr><td align="left"><input type="radio" name="group5" value="0"> No </td></tr>
 
 <tr><td align="left">Do you prefer a restaurant that has outside seating?</td></tr>
-<tr><td align="left"><input type="radio" name="group6" value="yesOutsideSeating"> Yes </td></tr>
-<tr><td align="left"><input type="radio" name="group6" value="noOutsideSeating"> No </td></tr>
+<tr><td align="left"><input type="radio" name="group6" value="1"> Yes </td></tr>
+<tr><td align="left"><input type="radio" name="group6" value="0"> No </td></tr>
 
 <tr><td align="left">Do you prefer a restaurant that has a full bar?</td></tr>
-<tr><td align="left"><input type="radio" name="group7" value="yesBar"> Yes </td></tr>
-<tr><td align="left"><input type="radio" name="group7" value="noBar"> No </td></tr>
+<tr><td align="left"><input type="radio" name="group7" value="1"> Yes </td></tr>
+<tr><td align="left"><input type="radio" name="group7" value="0"> No </td></tr>
 
 <tr><td align="left">Do you prefer a restaurant that is good for kids?</td></tr>
-<tr><td align="left"><input type="radio" name="group8" value="yesKids"> Yes </td></tr>
-<tr><td align="left"><input type="radio" name="group8" value="noKids"> No </td></tr>
+<tr><td align="left"><input type="radio" name="group8" value="1"> Yes </td></tr>
+<tr><td align="left"><input type="radio" name="group8" value="0"> No </td></tr>
 
 <tr><td align="left">On a scale of 1 to 5, how fast do you want your service to be?</td></tr>
-<tr><td align="left"><input type="radio" name="group9" value="speedOne"> 1 (Sluggish) </td></tr>
-<tr><td align="left"><input type="radio" name="group9" value="speedTwo"> 2 </td></tr>
-<tr><td align="left"><input type="radio" name="group9" value="speedThree"> 3 (Moderate)</td></tr>
-<tr><td align="left"><input type="radio" name="group9" value="speedFour"> 4 </td></tr>
-<tr><td align="left"><input type="radio" name="group9" value="speedFive"> 5 (Super fast)</td></tr>
+<tr><td align="left"><input type="radio" name="group9" value="1"> 1 (Sluggish) </td></tr>
+<tr><td align="left"><input type="radio" name="group9" value="2"> 2 </td></tr>
+<tr><td align="left"><input type="radio" name="group9" value="3"> 3 (Moderate)</td></tr>
+<tr><td align="left"><input type="radio" name="group9" value="4"> 4 </td></tr>
+<tr><td align="left"><input type="radio" name="group9" value="5"> 5 (Super fast)</td></tr>
 <tr> </tr><tr> </tr>
 <tr><td align="left">On a scale of 1 to 5, how much do you prefer this type of restaurant?</tr>
 <tr><td align="center"><b>Steakhouse</b></td></tr>
-<tr><td align="center"><input type="radio" name="group10" value="SteakOne"> 1 </td></tr>
-<tr><td align="center"><input type="radio" name="group10" value="SteakTwo"> 2 </td></tr>
-<tr><td align="center"><input type="radio" name="group10" value="SteakThree"> 3 </td></tr>
-<tr><td align="center"><input type="radio" name="group10" value="SteakFour"> 4 </td></tr>
-<tr><td align="center"><input type="radio" name="group10" value="SteakFive"> 5 </td></tr>
+<tr><td align="center"><input type="radio" name="group10" value="1"> 1 </td></tr>
+<tr><td align="center"><input type="radio" name="group10" value="2"> 2 </td></tr>
+<tr><td align="center"><input type="radio" name="group10" value="3"> 3 </td></tr>
+<tr><td align="center"><input type="radio" name="group10" value="4"> 4 </td></tr>
+<tr><td align="center"><input type="radio" name="group10" value="5"> 5 </td></tr>
 
 <tr><td align="center"><b>American</b></td></tr>
-<tr><td align="center"><input type="radio" name="group11" value="AmerOne"> 1 </td></tr>
-<tr><td align="center"><input type="radio" name="group11" value="AmerTwo"> 2 </td></tr>
-<tr><td align="center"><input type="radio" name="group11" value="AmerThree"> 3 </td></tr>
-<tr><td align="center"><input type="radio" name="group11" value="AmerFour"> 4 </td></tr>
-<tr><td align="center"><input type="radio" name="group11" value="AmerFive"> 5 </td></tr>
+<tr><td align="center"><input type="radio" name="group11" value="1"> 1 </td></tr>
+<tr><td align="center"><input type="radio" name="group11" value="2"> 2 </td></tr>
+<tr><td align="center"><input type="radio" name="group11" value="3"> 3 </td></tr>
+<tr><td align="center"><input type="radio" name="group11" value="4"> 4 </td></tr>
+<tr><td align="center"><input type="radio" name="group11" value="5"> 5 </td></tr>
 
 <tr><td align="center"><b>Middle Eastern</b></td></tr>
-<tr><td align="center"><input type="radio" name="group12" value="MEOne"> 1 </td></tr>
-<tr><td align="center"><input type="radio" name="group12" value="METwo"> 2 </td></tr>
-<tr><td align="center"><input type="radio" name="group12" value="METhree"> 3 </td></tr>
-<tr><td align="center"><input type="radio" name="group12" value="MEFour"> 4 </td></tr>
-<tr><td align="center"><input type="radio" name="group12" value="MEFive"> 5 </td></tr>
+<tr><td align="center"><input type="radio" name="group12" value="1"> 1 </td></tr>
+<tr><td align="center"><input type="radio" name="group12" value="2"> 2 </td></tr>
+<tr><td align="center"><input type="radio" name="group12" value="3"> 3 </td></tr>
+<tr><td align="center"><input type="radio" name="group12" value="4"> 4 </td></tr>
+<tr><td align="center"><input type="radio" name="group12" value="5"> 5 </td></tr>
 
 <tr><td align="center"><b>Asian</b></td></tr>
-<tr><td align="center"><input type="radio" name="group13" value="AsianOne"> 1 </td></tr>
-<tr><td align="center"><input type="radio" name="group13" value="AsianTwo"> 2 </td></tr>
-<tr><td align="center"><input type="radio" name="group13" value="AsianThree"> 3 </td></tr>
-<tr><td align="center"><input type="radio" name="group13" value="AsianFour"> 4 </td></tr>
-<tr><td align="center"><input type="radio" name="group13" value="AsianFive"> 5 </td></tr>
+<tr><td align="center"><input type="radio" name="group13" value="1"> 1 </td></tr>
+<tr><td align="center"><input type="radio" name="group13" value="2"> 2 </td></tr>
+<tr><td align="center"><input type="radio" name="group13" value="3"> 3 </td></tr>
+<tr><td align="center"><input type="radio" name="group13" value="4"> 4 </td></tr>
+<tr><td align="center"><input type="radio" name="group13" value="5"> 5 </td></tr>
 
 <tr><td align="center"><b>Italian</b></td></tr>
-<tr><td align="center"><input type="radio" name="group14" value="ItalianOne"> 1 </td></tr>
-<tr><td align="center"><input type="radio" name="group14" value="ItalianTwo"> 2 </td></tr>
-<tr><td align="center"><input type="radio" name="group14" value="ItalianThree"> 3 </td></tr>
-<tr><td align="center"><input type="radio" name="group14" value="ItalianFour"> 4 </td></tr>
-<tr><td align="center"><input type="radio" name="group14" value="ItalianFive"> 5 </td></tr>
+<tr><td align="center"><input type="radio" name="group14" value="1"> 1 </td></tr>
+<tr><td align="center"><input type="radio" name="group14" value="2"> 2 </td></tr>
+<tr><td align="center"><input type="radio" name="group14" value="3"> 3 </td></tr>
+<tr><td align="center"><input type="radio" name="group14" value="4"> 4 </td></tr>
+<tr><td align="center"><input type="radio" name="group14" value="5"> 5 </td></tr>
 
 <tr><td align="center"><b>Chinese</b></td></tr>
-<tr><td align="center"><input type="radio" name="group15" value="ChineseOne"> 1 </td></tr>
-<tr><td align="center"><input type="radio" name="group15" value="ChineseTwo"> 2 </td></tr>
-<tr><td align="center"><input type="radio" name="group15" value="ChineseThree"> 3 </td></tr>
-<tr><td align="center"><input type="radio" name="group15" value="ChineseFour"> 4 </td></tr>
-<tr><td align="center"><input type="radio" name="group15" value="ChineseFive"> 5 </td></tr>
+<tr><td align="center"><input type="radio" name="group15" value="1"> 1 </td></tr>
+<tr><td align="center"><input type="radio" name="group15" value="2"> 2 </td></tr>
+<tr><td align="center"><input type="radio" name="group15" value="3"> 3 </td></tr>
+<tr><td align="center"><input type="radio" name="group15" value="4"> 4 </td></tr>
+<tr><td align="center"><input type="radio" name="group15" value="5"> 5 </td></tr>
 
 <tr><td align="center"><b>Japanese</b></td></tr>
-<tr><td align="center"><input type="radio" name="group16" value="JapOne"> 1 </td></tr>
-<tr><td align="center"><input type="radio" name="group16" value="JapTwo"> 2 </td></tr>
-<tr><td align="center"><input type="radio" name="group16" value="JapThree"> 3 </td></tr>
-<tr><td align="center"><input type="radio" name="group16" value="JapFour"> 4 </td></tr>
-<tr><td align="center"><input type="radio" name="group16" value="JapFive"> 5 </td></tr>
+<tr><td align="center"><input type="radio" name="group16" value="1"> 1 </td></tr>
+<tr><td align="center"><input type="radio" name="group16" value="2"> 2 </td></tr>
+<tr><td align="center"><input type="radio" name="group16" value="3"> 3 </td></tr>
+<tr><td align="center"><input type="radio" name="group16" value="4"> 4 </td></tr>
+<tr><td align="center"><input type="radio" name="group16" value="5"> 5 </td></tr>
 
 <tr><td align="center"><b>Indian</b></td></tr>
-<tr><td align="center"><input type="radio" name="group17" value="IndianOne"> 1 </td></tr>
-<tr><td align="center"><input type="radio" name="group17" value="IndianTwo"> 2 </td></tr>
-<tr><td align="center"><input type="radio" name="group17" value="IndianThree"> 3 </td></tr>
-<tr><td align="center"><input type="radio" name="group17" value="IndianFour"> 4 </td></tr>
-<tr><td align="center"><input type="radio" name="group17" value="IndianFive"> 5 </td></tr>
+<tr><td align="center"><input type="radio" name="group17" value="1"> 1 </td></tr>
+<tr><td align="center"><input type="radio" name="group17" value="2"> 2 </td></tr>
+<tr><td align="center"><input type="radio" name="group17" value="3"> 3 </td></tr>
+<tr><td align="center"><input type="radio" name="group17" value="4"> 4 </td></tr>
+<tr><td align="center"><input type="radio" name="group17" value="5"> 5 </td></tr>
 
 <tr><td align="center"><b>French</b></td></tr>
-<tr><td align="center"><input type="radio" name="group18" value="FrenchOne"> 1 </td></tr>
-<tr><td align="center"><input type="radio" name="group18" value="FrenchTwo"> 2 </td></tr>
-<tr><td align="center"><input type="radio" name="group18" value="FrenchThree"> 3 </td></tr>
-<tr><td align="center"><input type="radio" name="group18" value="FrenchFour"> 4 </td></tr>
-<tr><td align="center"><input type="radio" name="group18" value="FrenchFive"> 5 </td></tr>
+<tr><td align="center"><input type="radio" name="group18" value="1"> 1 </td></tr>
+<tr><td align="center"><input type="radio" name="group18" value="2"> 2 </td></tr>
+<tr><td align="center"><input type="radio" name="group18" value="3"> 3 </td></tr>
+<tr><td align="center"><input type="radio" name="group18" value="4"> 4 </td></tr>
+<tr><td align="center"><input type="radio" name="group18" value="5"> 5 </td></tr>
 
 <tr><td align="center"><b>Greek</b></td></tr>
-<tr><td align="center"><input type="radio" name="group19" value="GreekOne"> 1 </td></tr>
-<tr><td align="center"><input type="radio" name="group19" value="GreekTwo"> 2 </td></tr>
-<tr><td align="center"><input type="radio" name="group19" value="GreekThree"> 3 </td></tr>
-<tr><td align="center"><input type="radio" name="group19" value="GreekFour"> 4 </td></tr>
-<tr><td align="center"><input type="radio" name="group19" value="GreekFive"> 5 </td></tr>
+<tr><td align="center"><input type="radio" name="group19" value="1"> 1 </td></tr>
+<tr><td align="center"><input type="radio" name="group19" value="2"> 2 </td></tr>
+<tr><td align="center"><input type="radio" name="group19" value="3"> 3 </td></tr>
+<tr><td align="center"><input type="radio" name="group19" value="4"> 4 </td></tr>
+<tr><td align="center"><input type="radio" name="group19" value="5"> 5 </td></tr>
 
 <tr><td align="center"><b>Mexican</b></td></tr>
-<tr><td align="center"><input type="radio" name="group20" value="MexicanOne"> 1 </td></tr>
-<tr><td align="center"><input type="radio" name="group20" value="MexicanTwo"> 2 </td></tr>
-<tr><td align="center"><input type="radio" name="group20" value="MexicanThree"> 3 </td></tr>
-<tr><td align="center"><input type="radio" name="group20" value="MexicanFour"> 4 </td></tr>
-<tr><td align="center"><input type="radio" name="group20" value="MexicanFive"> 5 </td></tr>
+<tr><td align="center"><input type="radio" name="group20" value="1"> 1 </td></tr>
+<tr><td align="center"><input type="radio" name="group20" value="2"> 2 </td></tr>
+<tr><td align="center"><input type="radio" name="group20" value="3"> 3 </td></tr>
+<tr><td align="center"><input type="radio" name="group20" value="4"> 4 </td></tr>
+<tr><td align="center"><input type="radio" name="group20" value="5"> 5 </td></tr>
 
 <tr><td align="center"><b>Vegetarian</b></td></tr>
-<tr><td align="center"><input type="radio" name="group21" value="VegOne"> 1 </td></tr>
-<tr><td align="center"><input type="radio" name="group21" value="VegTwo"> 2 </td></tr>
-<tr><td align="center"><input type="radio" name="group21" value="VegThree"> 3 </td></tr>
-<tr><td align="center"><input type="radio" name="group21" value="VegFour"> 4 </td></tr>
-<tr><td align="center"><input type="radio" name="group21" value="VegFive"> 5 </td></tr>
+<tr><td align="center"><input type="radio" name="group21" value="1"> 1 </td></tr>
+<tr><td align="center"><input type="radio" name="group21" value="2"> 2 </td></tr>
+<tr><td align="center"><input type="radio" name="group21" value="3"> 3 </td></tr>
+<tr><td align="center"><input type="radio" name="group21" value="4"> 4 </td></tr>
+<tr><td align="center"><input type="radio" name="group21" value="5"> 5 </td></tr>
 
 <tr><td align="center"><b>Seafood</b></td></tr>
-<tr><td align="center"><input type="radio" name="group22" value="SeafoodOne"> 1 </td></tr>
-<tr><td align="center"><input type="radio" name="group22" value="SeafoodTwo"> 2 </td></tr>
-<tr><td align="center"><input type="radio" name="group22" value="SeafoodThree"> 3 </td></tr>
-<tr><td align="center"><input type="radio" name="group22" value="SeafoodFour"> 4 </td></tr>
-<tr><td align="center"><input type="radio" name="group22" value="SeafoodFive"> 5 </td></tr>
+<tr><td align="center"><input type="radio" name="group22" value="1"> 1 </td></tr>
+<tr><td align="center"><input type="radio" name="group22" value="2"> 2 </td></tr>
+<tr><td align="center"><input type="radio" name="group22" value="3"> 3 </td></tr>
+<tr><td align="center"><input type="radio" name="group22" value="4"> 4 </td></tr>
+<tr><td align="center"><input type="radio" name="group22" value="5"> 5 </td></tr>
 </table>
 <table>
 <tr><td>&nbsp;</td><td align="center"><input type="submit" value="Create Account"></td></tr>
@@ -202,7 +225,7 @@ if ($_GET['signup'] == "yes") {
 <p>&nbsp;</p>
 <p>&nbsp;</p>
 <p>&nbsp;</p>
-</center>");
+</center>
 
 <?php
 include("footer.php");
